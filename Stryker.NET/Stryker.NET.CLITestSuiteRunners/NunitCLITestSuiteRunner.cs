36dbf0f51effd9d54af7d701ebbfcf27dab3bf7d
pavzaj <pavzaj@gmail.com>
2017-07-04 21:39:52 +0200

    Added CLI command executable file selector

---
+++ Stryker.NET/Stryker.NET.CLITestSuiteRunners/NunitCLITestSuiteRunner.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+
+namespace Stryker.NET.CLITestSuiteRunners
+{
+    public class NunitCLITestSuiteRunner
+    {
+        Process testRunningCLI;
+
+        protected virtual void ValidateCliCommand(string FullCLICommand)
+        {
+            if (FullCLICommand == null)
+                throw new ArgumentNullException("FullCLICommand");
+
+            if (FullCLICommand == string.Empty)
+                throw new ArgumentException("Should not be empty string", "FullCLICommand");
+        }
+
+        protected virtual string GetProcessExecutableNameFromCommand(string FullCLICommand)
+        {
+            if (string.IsNullOrEmpty(FullCLICommand))
+                return string.Empty;
+
+            if (FullCLICommand.StartsWith("\""))
+            {
+                int indexOfSecondQuoteMark = FullCLICommand.IndexOf('"', 1);
+
+                return FullCLICommand.Substring(0, indexOfSecondQuoteMark + 1);
+            }
+
+            int firstIndexOfSpace = FullCLICommand.IndexOf(' ');
+
+            if (firstIndexOfSpace < 0)
+                return FullCLICommand;
+
+            return FullCLICommand.Substring(0, firstIndexOfSpace);
+        }
+
+        protected virtual string GetProcessArgumentsFromCommand(string FullCLICommand)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Stryker.NET/_tests/Stryker.NET.CLITestSuiteRunners.UnitTests/NunitCLITestSuiteRunnerTests.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Stryker.NET.CLITestSuiteRunners.UnitTests
+{
+    public class NunitCLITestSuiteRunnerTests
+    {
+        public class ValidateCliCommand : NunitCLITestSuiteRunner
+        {
+            [Fact]
+            public void ValidateCliCommand_NullCommand_Exception()
+            {
+                string command = null;
+
+                Assert.Throws<ArgumentNullException>(() => { ValidateCliCommand(command); });
+            }
+
+            [Fact]
+            public void ValidateCliCommand_EmptyCommand_Exception()
+            {
+                string command = string.Empty;
+
+                Assert.Throws<ArgumentException>(() => { ValidateCliCommand(command); });
+            }
+        }
+
+        public class GetProcessExecutableNameFromCommand : NunitCLITestSuiteRunner
+        {
+            [Theory]
+            [InlineData("var x = 1 + 2;", "1 - 2")]
+            public void GetProcessExecutableNameFromCommand_NullCommand_EmptyStringOutput()
+            {
+                string command = null;
+                var expected = string.Empty;
+
+                var actual = GetProcessExecutableNameFromCommand(command);
+
+                Assert.Equal(expected, actual);
+            }
+
+            [Fact]
+            public void GetProcessExecutableNameFromCommand_EmptyCommand_EmptyStringOutput()
+            {
+                string command = string.Empty;
+                var expected = string.Empty;
+
+                var actual = GetProcessExecutableNameFromCommand(command);
+
+                Assert.Equal(expected, actual);
+            }
+
+            [Fact]
+            public void GetProcessExecutableNameFromCommand_OneWordCommand_SameWordReturned()
+            {
+                string command = "SomeProgramToRun.exe";
+                var expected = "SomeProgramToRun.exe";
+
+                var actual = GetProcessExecutableNameFromCommand(command);
+
+                Assert.Equal(expected, actual);
+            }
+
+            [Fact]
+            public void GetProcessExecutableNameFromCommand_TwoWordCommandSeparatedWithOneSpace_FirstWordWithoutSpacesReturned()
+            {
+                string command = "SomeProgramToRun.exe --parametersNotProvided";
+                var expected = "SomeProgramToRun.exe";
+
+                var actual = GetProcessExecutableNameFromCommand(command);
+
+                Assert.Equal(expected, actual);
+            }
+
+            [Fact]
+            public void GetProcessExecutableNameFromCommand_TwoWordCommandSeparatedWithManySpaces_FirstWordWithoutSpacesReturned()
+            {
+                string command = "SomeProgramToRun.exe           --parametersNotProvided";
+                var expected = "SomeProgramToRun.exe";
+
+                var actual = GetProcessExecutableNameFromCommand(command);
+
+                Assert.Equal(expected, actual);
+            }
+
+            [Fact]
+            public void GetProcessExecutableNameFromCommand_ManyWordCommandSeparatedWithManySpaces_FirstWordWithoutSpacesReturned()
+            {
+                string command = "SomeProgramToRun.exe           --parametersNotProvided yet   there  are     some";
+                var expected = "SomeProgramToRun.exe";
+
+                var actual = GetProcessExecutableNameFromCommand(command);
+
+                Assert.Equal(expected, actual);
+            }
+
+            [Fact]
+            public void GetProcessExecutableNameFromCommand_ManyWordCommandSeparatedWithManySpacesWithQuotedFullExecutablePathContainingSpace_FirstWordInQuotesWithoutSpacesOnTheEndReturned()
+            {
+                string command = "\"c:\\Program Files\\SomeProgramToRun.exe\"           --parametersNotProvided yet   there  are     some";
+                var expected = @"""c:\Program Files\SomeProgramToRun.exe""";
+
+                var actual = GetProcessExecutableNameFromCommand(command);
+
+                Assert.Equal(expected, actual);
+            }
+
+            [Fact]
+            public void GetProcessExecutableNameFromCommand_ManyWordCommandSeparatedWithManySpacesWithQuotedFullExecutablePathContainingSpaceAndQuotedParemeters_FirstWordInQuotesWithoutSpacesOnTheEndReturned()
+            {
+                string command = "\"c:\\Program Files\\SomeProgramToRun.exe\"           --parametersNotProvided \"yet   there  are     some\"";
+                var expected = @"""c:\Program Files\SomeProgramToRun.exe""";
+
+                var actual = GetProcessExecutableNameFromCommand(command);
+
+                Assert.Equal(expected, actual);
+            }
+        }
+    }
+}
