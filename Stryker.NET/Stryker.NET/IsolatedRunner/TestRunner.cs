312fdebecb83963851c32639c4c4ceb64d04761c
Yael Keemink <YaelK@infosupport.com>
2017-11-24 15:07:35 +0100

    Refactored some code

---
+++ Stryker.NET/Stryker.NET/IsolatedRunner/TestRunner.cs
@@ -0,0 +1,41 @@
+﻿using System.Diagnostics;
+using Stryker.NET.Managers;
+
+namespace Stryker.NET.IsolatedRunner
+{
+    public class TestRunner : ITestRunner
+    {
+        private readonly string _rootDirectory;        
+        private readonly string _tempDirectory;
+        private readonly string _command;
+
+        public TestRunner(string rootDir)
+        {
+            _rootDirectory = rootDir;
+            _tempDirectory = $"{_rootDirectory}\\Temp";
+            _command = "dotnet";
+            DirectoryManager.CreateDirectory(_tempDirectory);
+        }
+
+        public void Test()
+        {
+            var arguments = $"test";
+            RunCommand(arguments);
+
+        }
+
+        private void RunCommand(string arguments)
+        {
+            var info = new ProcessStartInfo(_command, arguments)
+            {
+                UseShellExecute = false,
+                WorkingDirectory = _tempDirectory
+            };
+            var process = Process.Start(info);            
+            while (!process.HasExited)
+            {
+                process = Process.GetProcessById(process.Id);
+            }
+        }
+    }
+}
---
+++ Stryker.NET/Stryker.NET/Managers/DirectoryManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Stryker.NET.Managers
+{
+    public class DirectoryManager
+    {
+        public static void CreateDirectory(string directory)
+        {
+            if (!Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+        }
+    }
+}
